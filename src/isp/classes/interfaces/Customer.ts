7230bdf593df3bbecc7afb483fe61d0dcec07760
raul0777 <raul.gome44@gmail.com>
2022-10-11 17:06:43 -0300

    ISP - Interface Segregation Principle(Princípio da Segurança de Interface)

---
+++ src/isp/classes/interfaces/Customer.ts
@@ -0,0 +1,43 @@
+import {
+  CustomerOrder,
+  EnterpriseCustomerProtocol,
+  IndividualCustomerProtocol,
+} from './customer-protocol';
+
+export class IndividualCustomer
+  implements IndividualCustomerProtocol, CustomerOrder
+{
+  firstName: string;
+  lasName: string;
+  cpf: string;
+
+  constructor(firstName: string, lasName: string, cpf: string) {
+    this.firstName = firstName;
+    this.lasName = lasName;
+    this.cpf = cpf;
+  }
+  getName(): string {
+    return this.firstName + ' ' + this.lasName;
+  }
+  getIDN(): string {
+    return this.cpf;
+  }
+}
+
+export class EnterpriseCustomer
+  implements EnterpriseCustomerProtocol, CustomerOrder
+{
+  name: string;
+  cnpj: string;
+
+  constructor(name: string, cnpj: string) {
+    this.name = name;
+    this.cnpj = cnpj;
+  }
+  getName(): string {
+    return this.name;
+  }
+  getIDN(): string {
+    return this.cnpj;
+  }
+}
--- src/isp/main.ts
+++ src/isp/main.ts
@@ -11,6 +11,12 @@ dever ser verdadeiro para objetos y de tipo S onde S é um subtipo de T.
 Mais simples: Subtipos precisam ser substituíveis por seus tipos de base.
 Mais simples ainda: Se um programa espera um Animal, algo do tipo Cachorro
 (que herde de Animal) deve servir como qualquer outro animal.
+
+###############
+
+Interface segregation principle (Princípio da segregação de Interface) -
+os clientes não devem ser forçado a depender de types, interfaces ou membros
+abstratos que não utilizam
 */
 import { ShoppingCart } from './classes/shopping-cart';
 import { Order } from './classes/order';
@@ -18,6 +24,10 @@ import { Messaging } from './services/messaging';
 import { Persistency } from './services/persistency';
 import { Product } from './classes/product';
 import { FiftyPercentDiscount } from './classes/discount';
+import {
+  EnterpriseCustomer,
+  IndividualCustomer,
+} from './classes/interfaces/Customer';
 
 const fiftyPercentDiscount = new FiftyPercentDiscount();
 // const temPercentDiscount = new TemPercentDiscount();
@@ -25,7 +35,17 @@ const fiftyPercentDiscount = new FiftyPercentDiscount();
 const shoppingCart = new ShoppingCart(fiftyPercentDiscount);
 const messaging = new Messaging();
 const persistency = new Persistency();
-const order = new Order(shoppingCart, messaging, persistency);
+//const individualCustomer = new IndividualCustomer('Raul', 'Gomes', '111.222.333-44',);
+const enterpriseCustomer = new EnterpriseCustomer(
+  'Empresa Gigante',
+  '11.222.333/4444-55',
+);
+const order = new Order(
+  shoppingCart,
+  messaging,
+  persistency,
+  enterpriseCustomer,
+);
 shoppingCart.addItem(new Product('Camiseta', 49.91));
 shoppingCart.addItem(new Product('Caderno', 9.9123));
 shoppingCart.addItem(new Product('Lápis', 1.59));
