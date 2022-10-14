e8d6917068e016e02290a78b4d7b0392c3f14e76
raul0777 <raul.gome44@gmail.com>
2022-10-14 18:15:25 -0300

    Asserções com Jest

--- src/services/persistency.spec.ts
+++ src/services/persistency.spec.ts
@@ -1,7 +1,13 @@
+/*
+sudo npm install -g jest
+npx jest --init
+####Comando####
+npx jest
+*/
 describe('TESTANDO ALGUMA COISA', () => {
   it('Should return one.(IT)', () => {
     const number = 1;
-    expect(number).not.toBe(1);
+    expect(number).toBe(1);
   });
 });
 
