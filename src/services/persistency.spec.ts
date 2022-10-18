aef92b23e524f05fd06f1da2240f09183f937294
raul0777 <raul.gome44@gmail.com>
2022-10-18 18:11:14 -0300

    Primeiro test com spyOn

--- src/services/persistency.spec.ts
+++ src/services/persistency.spec.ts
@@ -3,7 +3,7 @@ sudo npm install -g jest
 npx jest --init
 ####Comando####
 npx jest
-*/
+####Teste####
 describe('TESTANDO ALGUMA COISA', () => {
   it('Should return one.(IT)', () => {
     const number = 1;
@@ -15,5 +15,31 @@ describe('TESTANDO OUTRA COISA', () => {
   test('Should return Raul.(TESTE)', () => {
     const name = 'Raul';
     expect(name).toBe('Raul');
+  });
+});
+*/
+import { Persistency } from './persistency';
+
+describe('Persistency', () => {
+  afterEach(() => jest.clearAllMocks());
+
+  it('Should return undefined', () => {
+    //System under test
+    const sut = new Persistency();
+    expect(sut.saveOrder()).toBeUndefined();
+  });
+
+  it('Should call console.log once', () => {
+    const sut = new Persistency();
+    const consoleSpy = jest.spyOn(console, 'log');
+    sut.saveOrder();
+    expect(consoleSpy).toHaveBeenCalledTimes(1);
+  });
+
+  it('Should call console.log with "Pedido salvo com sucesso..."', () => {
+    const sut = new Persistency();
+    const consoleSpy = jest.spyOn(console, 'log');
+    sut.saveOrder();
+    expect(consoleSpy).toHaveBeenCalledWith('Pedido salvo com sucesso...');
   });
 });
