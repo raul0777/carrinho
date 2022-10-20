de060782e63db8d7a9d97b95c1a1ed0f0f58ec92
raul0777 <raul.gome44@gmail.com>
2022-10-20 17:23:34 -0300

    Testando ShoppingCart

---
+++ src/classes/shopping-cart.spect.ts
@@ -0,0 +1,66 @@
+import { ShoppingCart } from './shopping-cart';
+import { Discount } from './discount';
+import { CartItem } from './interfaces/cart-item';
+
+const createSut = () => {
+  const discountMock = createDiscountMock();
+  const sut = new ShoppingCart(discountMock);
+  return { sut, discountMock };
+};
+
+const createDiscountMock = () => {
+  class DiscountMock extends Discount {}
+  return new DiscountMock();
+};
+
+const createCartItem = (name: string, price: number) => {
+  class CartItemMock implements CartItem {
+    constructor(public name: string, public price: number) {}
+  }
+
+  return new CartItemMock(name, price);
+};
+
+const createSutWithProducts = () => {
+  const { sut, discountMock } = createSut();
+  const cartItem1 = createCartItem('Camiseta', 40);
+  const cartItem2 = createCartItem('Caneta', 1);
+  sut.addItem(cartItem1);
+  sut.addItem(cartItem2);
+  return { sut, discountMock };
+};
+
+describe('ShoppingCart', () => {
+  it('Should be an empty cart when no product is added', () => {
+    const { sut } = createSut();
+    expect(sut.isEmpty()).toBe(true);
+  });
+
+  it('Should have 2 cart items', () => {
+    const { sut } = createSutWithProducts();
+    expect(sut.items.length).toBe(1);
+  });
+
+  it('Should test total and totalWithDiscount', () => {
+    const { sut } = createSutWithProducts();
+    expect(sut.total()).toBe(41);
+    expect(sut.totalWithDiscount()).toBe(41);
+  });
+
+  it('Should add products and clear cart', () => {
+    const { sut } = createSutWithProducts();
+    expect(sut.items.length).toBe(1);
+    sut.clear();
+    expect(sut.items.length).toBe(0);
+    expect(sut.isEmpty()).toBe(true);
+  });
+
+  it('Should remove products', () => {
+    const { sut } = createSutWithProducts();
+    expect(sut.items.length).toBe(2);
+    sut.removeItem(1);
+    expect(sut.items.length).toBe(1);
+    sut.removeItem(0);
+    expect(sut.isEmpty()).toBe(true);
+  });
+});
